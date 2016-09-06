4bfcbc6335c62aa5179784b115e18a62088f36e3
Susan Graves <susan@susangravescpa.com>
2016-09-06 15:30:52 -0400

    Winning numbers return my winner in array

--- test_winning_number.rb
+++ test_winning_number.rb
@@ -10,15 +10,28 @@ class TestArrayFunction < Minitest::Test
 #This are the test methods
 	
 
-	def test_to_check_for_winner
+	# def test_to_check_for_winner
+	# 	winners = ["1234", "2567", "1984", "3756"]
+	# 	my_num = "2567"
+	# 	assert_equal(true, result(winners, my_num))
+	# end
+
+	def test_to_check_for_winner_return_array
 		winners = ["1234", "2567", "1984", "3756"]
 		my_num = "2567"
-		assert_equal(true, result(winners, my_num))
+		assert_equal(["2567"], result(winners, my_num))
 	end
 
-	def test_1_to_check_for_winner
+	def test_to_check_for_winner_return_array_empty
 		winners = ["1234", "2567", "1984", "3756"]
 		my_num = "4567"
-		assert_equal(false, result(winners, my_num))
-	end
+		assert_equal([], result(winners, my_num))
+	end	
+
+
+	# def test_1_to_check_for_winner
+	# 	winners = ["1234", "2567", "1984", "3756"]
+	# 	my_num = "4567"
+	# 	assert_equal(false, result(winners, my_num))
+	# end
 end
--- winning_number.rb
+++ winning_number.rb
@@ -1,7 +1,10 @@
-#def check_for_winner
 
 def result(winners, my_num)	
-
-	winners.include?(my_num)
-		
+	mywinningnumbers = []
+		if winners.include?(my_num)
+			mywinningnumbers << my_num
+		end
+	mywinningnumbers
 end
+
+
